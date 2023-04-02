3a6691cc8ecf74fe0604b827d320beede783bd09
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2023-04-02 17:15:53 +0300

    create base, registration and login views

--- Auth_project/users/views.py
+++ Auth_project/users/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth, messages
+from django.urls import reverse
 
-# Create your views here.
+from .forms import UserLoginForm, UserRegistrationForm
+
+
+def base(request):
+    return render(request, 'users/base.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                messages.success(request, 'Успешный вход!')
+                return HttpResponseRedirect(reverse('base'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегистрированы!')
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
